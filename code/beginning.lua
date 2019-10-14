3d9d6a1f7284c006aeaa730a36ef439999b8ef86
chiara <chiara.volonnino@studio.unibo.it>
2019-10-14 12:10:47 +0200

    Add see light and fire function.

--- code/beginning.lua
+++ code/beginning.lua
@@ -3,75 +3,101 @@
 MOVE_STEPS = 50
 n_steps = 0
 steps_resolution = 5
-robotNumber = 0
+robot_number = 0
+light_sensor = nil
+motor_ground = nil
 
 --[[ This function is executed every time you press the 'execute'
      button ]]
 function init()
     n_steps = 0
 	robot.leds.set_all_colors("black")
-
-    max_intensity = 0
-    max_light_sensor_index = 1
+	robot.range_and_bearing.set_data(1, 1)
 end
 
 function step()
-	robot.range_and_bearing.set_data(1,10)	
+	robot_number = countRAB()	
+	--log("range_and_bearing " .. robot_number)
 	if n_steps % steps_resolution == 0 then
     	steps_resolution = DEFAULT_STEPS_RESOLUTION
-		wander()
+		follow_light()
   	end
   	n_steps = n_steps + 1
 end
 
-
-
 function wander() 
-	log("Wander state")
+	--log("Robot is in wander state")
 	steps_resolution = 20 
-  	robot.wheels.set_velocity(robot.random.uniform(10), robot.random.uniform(10))
-	robotNumber = countRAB()
-	log("Robot number:" .. robotNumber)
+	robot.range_and_bearing.set_data(1, 0)
+	robot.wheels.set_velocity(robot.random.uniform(10), robot.random.uniform(10))
 end 
 
-function followLight() 
+function follow_light()
+	if not see_light() then
+		wander()
+		robot.leds.set_all_colors("black")
+		if see_fire() then
+			log("I SEE FIRE")
+		end
+	else 
+		-- gestire follow the light secondo un modello definito
+		log("follow the light")
+		if see_fire() then
+			log("I SEE FIRE")
+		end
+	end
 end
 
-function obstaclesAvoid()
+function obstacles_avoid()
 end
 
-function stopNearFire()
+function stop_near_fire()
 end
 
---[[ This function is executed every time you press the 'reset'
-     button in the GUI. It is supposed to restore the state
-     of the controller to whatever it was right after init() was
-     called. The state of sensors and actuators is reset
-     automatically by ARGoS. ]]
-function reset()
-	left_v = robot.random.uniform(0,15)
-	right_v = robot.random.uniform(0,15)
-	robot.wheels.set_velocity(left_v,right_v)
-	n_steps = 0
-	robot.leds.set_all_colors("black")
+function see_light()
+	for _, light_sensor in pairs(robot.light) do
+		if light_sensor.value > 0 then 
+			robot.leds.set_all_colors("yellow")
+			return true
+		end 
+	end 
+	return false
+end
+
+function see_fire() 
+	for _, motor_ground in pairs(robot.motor_ground) do
+		log("motor_ground.value " .. motor_ground.value)
+		if robot.motor_ground[_].value == 0.29899998085172 then 
+			robot.leds.set_all_colors("red")
+			return true
+		elseif robot.motor_ground[_].value == 0.69493725346584 then
+			robot.leds.set_all_colors("blue")
+			return true
+		else 
+			log("don't see nothing")
+			return false
+		end
+	end
 end
 
 function countRAB()
-	log("countRAB")
 	number_robot_sensed = 0
 	-- for each robot seen
 	for _, rab in ipairs(robot.range_and_bearing) do
 		if rab.range < 30 and rab.data[1] == 1 then
 			number_robot_sensed = number_robot_sensed + 1
-			log("in :" .. number_robot_sensed)
+			--log("in :" .. number_robot_sensed)
 		end
 	end
-	log("in RAB:" .. number_robot_sensed)
 	return number_robot_sensed
 end 
 
---[[ This function is executed only once, when the robot is removed
-     from the simulation ]]
+function reset()
+	robot.wheels.set_velocity(0,0)
+	n_steps = 0
+	robot.leds.set_all_colors("black")
+end
+
 function destroy()
    -- put your code here
 end
