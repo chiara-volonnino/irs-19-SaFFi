dc82bfdd773e08cf87b1e59f6e49a13d5752911d
chiara <chiara.volonnino@studio.unibo.it>
2019-10-24 11:16:08 +0200

    Code cleaning.

--- code/beginning.lua
+++ code/beginning.lua
@@ -1,27 +1,21 @@
 local vector = require "vector"
 
-local SEARCH_FIRE = 0
-local ATTEMPT_NEIGHBOURS = 1
-local DEAL_FIRE = 2
-
-local GREEN_LED = "green"
-local RED_LED = "red"
-local BLACK = "black"
+local SEARCH_FIRE, ATTEMPT_NEIGHBOURS, DEAL_FIRE = 0, 1, 2
+local GREEN_LED, RED_LED, BLACK_LED = "green", "red", "black"
+local ROBOT_PRESENCE, ANTENNA_FIELD, ROBOT_FIELD, ANTENNA_COMUNICATION = 3, 4, 5, 6
+local MIN_WHEELS, MAX_WHEELS = 1, 10
 
 local motor_ground = nil
-
-local robot_state = 0  -- 0 = find light, 1 = reached fire, 2 = following robot, 3 = fleeing from fire
-
-local v1 = {}
-local v2 = {}
+local robot_state = 0
+local drift_vector, constrol_vector, behaviors_vector = {}, {}, {}
 
 function init()
 	distance_between_2_wheels = robot.wheels.axis_length
-	robot.leds.set_all_colors("black")
+	robot.leds.set_all_colors(BLACK_LED)
 end
 
 function reset()
-	robot.leds.set_all_colors("black")
+	robot.leds.set_all_colors(BLACK_LED)
 	robot.wheels.set_velocity(0,0)
 end
 
@@ -31,26 +25,25 @@ end
 
 function step()
 	if robot_state == SEARCH_FIRE then            
-		v1 = vector.vec2_polar_sum(wander(), avoid_obstacle())
-        v2 = vector.vec2_polar_sum(stop_near_fire(), read_range_and_bearing(4))
-        v3 = vector.vec2_polar_sum(v1, v2)
-        wheels_l, wheels_r = trasformation_vector_to_velocity(v3)
-        robot.leds.set_all_colors("white")
-		robot.wheels.set_velocity(wheels_normalization(wheels_l, 1, 10), wheels_normalization(wheels_r, 1, 10))
+		drift_vector = vector.vec2_polar_sum(wander(), avoid_obstacle())
+        constrol_vector = vector.vec2_polar_sum(stop_near_fire(), read_range_and_bearing())
+        behaviors_vector = vector.vec2_polar_sum(drift_vector, constrol_vector)
+        wheels_l, wheels_r = trasformation_vector_to_velocity(behaviors_vector)
+		robot.wheels.set_velocity(wheels_normalization(wheels_l), wheels_normalization(wheels_r))
 		if get_temperature_readings() then
-            robot.leds.set_all_colors("red")
+            robot.leds.set_all_colors(RED_LED)
 			robot_state = ATTEMPT_NEIGHBOURS
 		end
 	elseif robot_state == ATTEMPT_NEIGHBOURS then
         write_range_and_bearing(3,1)
-        write_range_and_bearing(5,1)
+        write_range_and_bearing(ROBOT_FIELD,1)
 		robot.wheels.set_velocity(0, 0)
         if check_antenna() then
-            robot.leds.set_all_colors("green")
+            robot.leds.set_all_colors(GREEN_LED)
             robot_state = DEAL_FIRE
         end
     elseif robot_state == DEAL_FIRE then
-        write_range_and_bearing(5,2)
+        write_range_and_bearing(ROBOT_FIELD,2)
 		robot.wheels.set_velocity(0, 0)
 	else 
 		--log("robot in state n")
@@ -86,27 +79,16 @@ function avoid_obstacle()
 end    
 
 function stop_near_fire()
-    attract = false
-    repulse = false
-    rabbi = nil
-    for _,rab in ipairs(robot.range_and_bearing) do
-        if rab.data[5] == 1 and not repulse then  
-            attract = true 
-            rabbi = rab
-        elseif rab.data[5] == 2 and rab.range < 40 then
-            repulse = true
-            rabbi = rab
-        end
-    end 
-    if not check_antenna() and attract then
+    neighbors_result = check_neighbors(ROBOT_FIELD)
+    if not check_antenna() and neighbors_result.attract_field then
         return {
-            length = range_and_bearing_normaliation(rabbi.range) * 2,  
-            angle = rabbi.horizontal_bearing
+            length = range_and_bearing_normaliation(neighbors_result.rab.range) * 2,  
+            angle = neighbors_result.rab.horizontal_bearing
         }
     elseif check_antenna() and repulse then
         return {
-            length = range_and_bearing_normaliation(rabbi.range) * 4,  
-            angle = rabbi.horizontal_bearing - math.pi
+            length = range_and_bearing_normaliation(neighbors_result.rab.range) * 4,  
+            angle = neighbors_result.rab.horizontal_bearing - math.pi
         }
     else
         return {
@@ -115,6 +97,7 @@ function stop_near_fire()
         }
     end
 end 
+
 -------------- Extra functions --------------
 function trasformation_vector_to_velocity(v)
 	v_left = v.length - ((v.angle * distance_between_2_wheels)/2)
@@ -177,17 +160,36 @@ end
 
 function check_antenna()
     for _,rab in ipairs(robot.range_and_bearing) do
-        if rab.data[6] == 1 and rab.range < 50 then
+        if rab.data[ANTENNA_COMUNICATION] == 1 and rab.range < 50 then
             return true
         end
     end
     return false
 end
 
+function check_neighbors(range_and_bearing_data)
+    attract_field, repulse_field = false, false
+    effective_rab = nil
+    for _,rab in ipairs(robot.range_and_bearing) do
+        if rab.data[range_and_bearing_data] == 1 and not repulse_field then  
+            attract_field = true 
+            effective_rab = rab
+        elseif rab.data[range_and_bearing_data] == 2 and rab.range < 40 then
+            repulse_field = true
+            effective_rab = rab
+        end
+    end
+    return {
+        rab = effective_rab, 
+        attract_field = attract_field, 
+        repulse_field = repulse_field
+    }
+end
+
 function range_and_bearing_normaliation(value) 
     return 1 - 1 / value
 end
 
-function wheels_normalization(value, min_value, max_value)
-    return  ((value - min_value) / (max_value - min_value)) * 10
+function wheels_normalization(value)
+    return  ((value - MIN_WHEELS) / (MAX_WHEELS - MIN_WHEELS)) * 10
 end
